9550a2350044fd79898b50596929b4d84d7c270c
Tiffany Guan <mintaffy@Tiffanys-MacBook-Pro-2.local>
2024-04-06 02:11:16 -0700

    working on part 5

--- ASG0/asg0.js
+++ ASG0/asg0.js
@@ -1,3 +1,8 @@
+// Tiffany Guan
+// CruzID: tijguan
+// ID: #1882244
+// CSE 160 Asg0
+// Spring 2024
 
 
 // drawVector()
@@ -13,6 +18,8 @@
 
     // source: https://www.javatpoint.com/how-to-draw-a-line-using-javascript#:~:text=ct_var.strokeStyle%C2%A0%3D%C2%A0%27yellow%27%3B%C2%A0%C2%A0
     ctx.beginPath();
+
+    // prob better to use (canvas.width / 2) and (canvas.height / 2) for a more conventional way oops
     ctx.moveTo(200,200);
     ctx.lineTo(200 + (v.elements[0]*20), 200 - (v.elements[1]*20));
 
@@ -41,22 +48,47 @@
     
     ctx.clearRect(0, 0, 400, 400);
 
-    var x = document.getElementById('xcoord').value;
-    var y = document.getElementById('ycoord').value;
+    // retrieve v1 coordinates
+    var x1 = document.getElementById('xcoord1').value;
+    var y1 = document.getElementById('ycoord1').value;
 
-    if (!x) {
-        console.log("Failed to retrieve vector's x-coordinate");
+    // retrieve v2 coordinates
+    var x2 = document.getElementById('xcoord2').value;
+    var y2 = document.getElementById('ycoord2').value;
+
+    if (!x1) {
+        console.log("Failed to retrieve v1's x-coordinate");
     }
-    if (!y) {
-        console.log("Failed to retrieve vector's y-coordinate");
+    if (!y1) {
+        console.log("Failed to retrieve v1's y-coordinate");
+    }
+    if (!x2) {
+        console.log("Failed to retrieve v2's x-coordinate");
+    }
+    if (!y2) {
+        console.log("Failed to retrieve v2's y-coordinate");
     }
 
-    var v1 = new Vector3([x, y, 0]);
+    var v1 = new Vector3([x1, y1, 0]);
+    var v2 = new Vector3([x2, y2, 0]);
     drawVector(v1, "red");
-    
-
-
+    drawVector(v2, "blue");
  }
+
+ // handleDrawOperationEvent()
+ // Function that draws new vectors (v3, or v1 and v2) in green 
+ // depending on which operation/scalars were used.
+ 
+function handleDrawOperationEvent() {
+    var canvas = document.getElementById('example');
+    var ctx = canvas.getContext('2d');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+
+    // zzzzz
+}
 
  // DrawRectangle.js
  // Function to draw a vector on the canvas
@@ -85,10 +117,10 @@
     var v1 = new Vector3([2.25, 2.25, 0]);
     drawVector(v1, "red");
 
-    // Part 3:
+    // Part 3 and Part 4: ( done in .html file and handleDrawEvent() function)
     // if button is clicked: calls handleDrawEvent() --> done in the HTML file
     
-    // Part 4:
+    // Part 5:
     // 
 
 }
