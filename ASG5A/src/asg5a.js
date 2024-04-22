3b6ad60124646a88ad1b8f9d756987ee4fd4ffa7
Tiffany Guan <mintaffy@Tiffanys-MacBook-Pro-2.local>
2024-04-21 19:03:00 -0700

    updated w/ addons and folders

--- ASG5A/src/asg5a.js
+++ ASG5A/src/asg5a.js
@@ -1,4 +1,8 @@
-import * as THREE from 'three';
+import * as THREE from '../lib/three.module.js';
+// import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+// import { OBJLoader } from 'three/jsm/loaders/OBJLoader.js';
+// import {MTLLoader} from 'three/jsm/loaders/MTLLoader.js';
+
 
 function main() {
 
@@ -6,11 +10,11 @@ function main() {
 	const renderer = new THREE.WebGLRenderer( { antialias: true, canvas } );
 
 	const fov = 75;
-	const aspect = 1.6; // the canvas default: width/height
-	const near = 0.1;
-	const far = 5;
+	const aspect = 1.6; // the canvas default: width/height = 300/150 = 2
+	const near = 0.5;
+	const far = 10;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.z = 2;
+	camera.position.z = 4.5;
 
 	const scene = new THREE.Scene();
 
@@ -24,6 +28,29 @@ function main() {
 		scene.add( light );
 
 	}
+    // MAKE A SPHERE?
+    // const sphereScene = new THREE.Scene();
+    const geoSphere = new THREE.SphereGeometry( 0.5, 32,  16, 0, Math.PI *3, 0, Math.PI); 
+    geoSphere.translate(1, 1.5, 0.5);
+    const materialSphere = new THREE.MeshBasicMaterial( { color: 0xffff00 } ); 
+    const sphere = new THREE.Mesh( geoSphere, materialSphere ); 
+    scene.add( sphere );
+
+    // to load fish obj file? errr it's not working..
+    // {
+    //     const mtlLoader = new MTLLoader();
+    //     mtlLoader.load('Fish/Goldfish_01.mtl', (mtl) => {
+    //         mtl.preload();
+    //         objLoader.setMaterials(mtl);
+        
+    //         const objLoader = new OBJLoader();
+    //         objLoader.load( 'Fish/Goldfish_01.obj', ( root ) => {
+
+    //             scene.add( root );
+
+    //         } );
+    //     } );
+	// }
 
 	const boxWidth = 0.5; // used to be all 1
 	const boxHeight = 0.5;
@@ -48,6 +75,7 @@ function main() {
 		makeInstance( geometry, 0x44aa88, 0 ), // greenish blue
 		makeInstance( geometry, 0x8844aa, - 2 ),
 		makeInstance( geometry, 0xaa8844, 2 ),
+       // makeInstance( geoSphere, 0xffff00, 1),
 	];
 
 	function render( time ) {
